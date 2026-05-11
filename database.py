f4adf615cca2c0f1793e8a033c8cca727b474f54
11410120240181@stud.cu.edu.eg <11410120240181@stud.cu.edu.eg>
2026-05-11 23:25:04 +0300

    update

--- database.py
+++ database.py
@@ -1,16 +1,19 @@
 import pyodbc
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 connection_string = (
-    r'DRIVER={ODBC Driver 18 for SQL Server};'
-    r'SERVER=Dodo-PC;'
-    r'DATABASE=PublisherDB;'
-    r'Trusted_Connection=yes;'
-    r'Encrypt=yes;'
-    r'TrustServerCertificate=yes;'
+    f'DRIVER={{ODBC Driver 18 for SQL Server}};'
+    f'SERVER={os.getenv("DB_SERVER")};'
+    f'DATABASE={os.getenv("DB_NAME")};'
+    f'Trusted_Connection=yes;'
+    f'Encrypt=yes;'
+    f'TrustServerCertificate=yes;'
 )
 
 def get_db():
-    """Dependency: opens a fresh connection per request, closes it when done."""
     conn = pyodbc.connect(connection_string)
     try:
         yield conn
