43a4dd29e5df300cf9276055020c9cb67c5f52f9
HazemKesba <hazemkesba@gmail.com>
2026-05-11 22:59:57 +0300

    Finished analytics route

--- routes/analytics.py
+++ routes/analytics.py
@@ -4,6 +4,23 @@ import pyodbc
 
 router = APIRouter()
 
+
 @router.get("/")
 async def get(conn: pyodbc.Connection = Depends(get_db)):
-    return { "message": "running" }
+    cursor = conn.cursor()
+    cursor.execute("Select Book.isbn, "+
+                   "Book.title, "+
+                   "Book.genre, "+
+                   "SUM([Order].quantity * (Format.price - Format.cost)) as total_price, "+
+                   "SUM([Order].quantity) as total_quantity " +
+                   "From Book, Format, [Order] " +
+                   "Where Book.isbn = Format.isbn and Format.format_id = [Order].format_id " +
+                   "Group By Book.isbn, Book.title, Book.genre " + 
+                   "Order By total_price desc")
+    column = [column[0] for column in cursor.description]
+
+    result = []
+    for row in cursor.fetchall():
+        result.append(dict(zip(column, row)))
+    
+    return result
